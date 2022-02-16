090ed39ba1541d5ea7900662363c4e4d13e25d86
‘classicoman2’ <aamengual@cifpfbmoll.eu>
2022-02-16 18:15:29 +0100

    classe dimecres 16-2-2022

---
+++ src/components/Contents.vue
@@ -0,0 +1,51 @@
+<template>
+  <p>Soy el componente HELLO-WORLD</p>
+  <h2>{{titol}}</h2>
+ 
+<div class="llista-classe">
+  <div v-for="item of items" v-bind:key="item">
+    {{convertirMajuscules(item)}}
+  </div>
+
+</div>
+
+<button class="btn btn-danger">Cambiar</button>
+
+</template>
+
+<script>
+export default {
+  name: 'Contents',
+  props: {
+    titol: String
+  },
+  data() {
+    return {
+      items: ["Pete", "Mark", "Jamie"]
+    }
+  },
+  methods: {
+    convertirMajuscules(text) {
+      return text.toUpperCase()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Contents titol="Llista de la Classe"/>
   </div>
 </template>
 
+
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Contents from '@/components/Contents.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Contents
   }
 }
 </script>
+
